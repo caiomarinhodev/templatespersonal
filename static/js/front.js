f963068984548cc843dfb346a20b516d1de5f043
Caio Marinho <caio.barbosa@embedded.ufcg.edu.br>
2016-04-26 12:46:42 -0300

    insert new features

--- static/js/front.js
+++ static/js/front.js
@@ -49,8 +49,8 @@ function map() {
     var styles = [{"featureType": "landscape", "stylers": [{"saturation": -100}, {"lightness": 65}, {"visibility": "on"}]}, {"featureType": "poi", "stylers": [{"saturation": -100}, {"lightness": 51}, {"visibility": "simplified"}]}, {"featureType": "road.highway", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "road.arterial", "stylers": [{"saturation": -100}, {"lightness": 30}, {"visibility": "on"}]}, {"featureType": "road.local", "stylers": [{"saturation": -100}, {"lightness": 40}, {"visibility": "on"}]}, {"featureType": "transit", "stylers": [{"saturation": -100}, {"visibility": "simplified"}]}, {"featureType": "administrative.province", "stylers": [{"visibility": "off"}]}, {"featureType": "water", "elementType": "labels", "stylers": [{"visibility": "on"}, {"lightness": -25}, {"saturation": -100}]}, {"featureType": "water", "elementType": "geometry", "stylers": [{"hue": "#ffff00"}, {"lightness": -25}, {"saturation": -97}]}];
     map = new GMaps({
         el: '#map',
-        lat: -12.043333,
-        lng: -77.028333,
+        lat: -7.2555557,
+        lng: -35.924030,
         zoomControl: true,
         zoomControlOpt: {
             style: 'SMALL',
@@ -65,11 +65,11 @@ function map() {
         styles: styles
     });
 
-    var image = 'img/marker.png';
+    var image = 'static/img/marker.png';
 
     map.addMarker({
-        lat: -12.043333,
-        lng: -77.028333,
+        lat: -7.2555557,
+        lng: -35.924030,
         icon: image,
         title: '',
         infoWindow: {
